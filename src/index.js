3045f2987c5ac3873fac70ec8239730091b23c71
ShajedaParvin <87864291+ShajedaParvin@users.noreply.github.com>
2022-10-30 04:12:14 +0600

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; 
+import LoginPage from './LoginPage/LoginPage';
+
+ReactDOM.render( 
+    <LoginPage/> ,
+  document.getElementById('root')
+);
